e2b06abfd595ff88bc0977011819121fbc0d2b85
Zorex Salvo <zorexsalvo@gmail.com>
2017-07-09 22:09:52 +0800

    Create CORS middleware
    Having problem on the js on frontend side because of
    Access-Control-Allow-Origin.

--- ideya/settings.py
+++ ideya/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'backend.middleware.corsmiddleware.CORSMiddleware',
 ]
 
 ROOT_URLCONF = 'ideya.urls'
