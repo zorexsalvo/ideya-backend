b9f4db783d3fe1d45f5fc0d984875a7f48ae24c4
Zorex Salvo <zorexsalvo@gmail.com>
2017-07-09 23:49:01 +0800

    Add content type to allowed headers

--- backend/middleware/corsmiddleware.py
+++ backend/middleware/corsmiddleware.py
@@ -5,5 +5,5 @@ class CORSMiddleware(object):
     def __call__(self, request):
         response = self.get_response(request)
         response["Access-Control-Allow-Origin"] = "*"
-
+        response["Access-Control-Allow-Headers"] = "Content-Type"
         return response
